ca4c4d480d5e9ee78c963157770d5e104dc97ebb
MetaServer\Deyter <csnix@yandex.ru>
2017-04-01 15:40:56 +0600

    first commit

---
+++ src/main/java/com/tigersoft/FurryYearsApplication.java
@@ -0,0 +1,12 @@
+package com.tigersoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryYearsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurryYearsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/tigersoft/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.tigersoft;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Deyter on 30.03.2017.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+    }
+}
